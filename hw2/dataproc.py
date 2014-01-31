1d40d8e93f39bc7b818cd3dc7a23c3104f740f69
jaberkow <jackberkowitz88@gmail.com>
2014-01-30 23:41:49 -0800

    data processing

---
+++ hw2/dataproc.py
@@ -0,0 +1,36 @@
+"""
+This module contains functions that take in the sentence and label data from
+.txt files and returns the data as lists of lists.  The top level list will is
+a list of the sentences or corresponding label sequences, and each entry in such
+list is a list of the words or labels in that sentence.
+"""
+
+def import_sentences(path_to_file):
+    """
+    path_to_file should be the path and filename of the sentences data.  Returns
+    a list of lists.
+    """
+    sentences = []
+    f = open(path_to_file,'r')
+    for line in f:
+        a = line.replace('\n','')
+        b = a.split()
+        sentences.append(b)
+    f.close()
+    return sentences
+
+def import_labels(path_to_file):
+    """
+    path_to_file should be the path and filename of the labels data.  Returns
+    a list of lists.
+    """
+    labels = []
+    f = open(path_to_file,'r')
+    for line in f:
+        a = line.replace('\n','')
+        b = a.split()
+        b += ['STOP']
+        b[:0] = ['START']
+        labels.append(b)
+    f.close()
+    return labels
