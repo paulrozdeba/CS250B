dac0bc3bba5fd94d79cdb31f1fbc29acf86d9765
jaberkow <jackberkowitz88@gmail.com>
2014-02-03 15:58:38 -0800

    added Z function to subroutines

--- hw2/subroutines.py
+++ hw2/subroutines.py
@@ -117,9 +117,13 @@ def __U_singlek__(g, k):
     else:
         return np.amax(__U_singlek__(g,k-1) + g[k], axis=0)
 
-def Z(w,x):
-    Z = 0.0
-    
+def Z(g):
+    alpha_matrix = alpha_mat(g)
+    beta_matrix = beta_mat(g)
+    Z = np.zeros(alpha_matrix.shape[0])
+    for k in range(alpha_matrix.shape[0]):
+        Z[k] = np.dot(alpha_matrix[k,:],beta_matrix[k,:])
+    return np.mean(Z)
     
 
 
