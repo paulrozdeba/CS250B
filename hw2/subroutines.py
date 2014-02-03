021c46603694515d1f382ed29828b81985b569df
Paul Rozdeba <paul.rozdeba@gmail.com>
2014-02-03 13:30:35 -0800

    modified g, got rid of hard dependency on start=y0 or stop=y(n+1)

--- hw2/subroutines.py
+++ hw2/subroutines.py
@@ -25,12 +25,15 @@ def g(f, w, tags, x):
     __g__ = np.zeros(shape=(N-1,M,M))
     
     for i,(k1,tag1),(k2,tag2),(j,weight) in it.product(range((1,N)),enumerate(tags),enumerate(tags),enumerate(w)):
-        if i == 1: 
-            __g__[i-1,k1,k2] += weight * f('START',tag2,x,i,j)
-        elif i == (N-1):
-            __g__[i-1,k1,k2] += weight * f(tag1,'STOP',x,i,j)
-        else:
-            __g__[i-1,k1,k2] += weight * f(tag1,tag2,x,i,j)
+#        if i == 1: 
+#            #__g__[i-1,k1,k2] += weight * f('START',tag2,x,i,j)
+#            __g__[i-1,k1,k2] += weight * f(tag1,tag2,x,i,j) * (tag1=='START')
+#        elif i == (N-1):
+#            #__g__[i-1,k1,k2] += weight * f(tag1,'STOP',x,i,j)
+#            __g__[i-1,k1,k2] += weight * f(tag1,tag2,x,i,j) * (tag2=='STOP')
+#        else:
+#            __g__[i-1,k1,k2] += weight * f(tag1,tag2,x,i,j)
+        __g__[i-1,k1,k2] += weight * f(tag1,tag2,x,i,j)
     
     return __g__
 
