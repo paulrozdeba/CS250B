35cce81447a8fd232f9fa2345757802e72c1396f
Paul Rozdeba <paul.rozdeba@gmail.com>
2014-02-03 15:36:51 -0800

    started on collins code, added meta feature function, finished g

---
+++ hw2/collins.py
@@ -0,0 +1,43 @@
+"""
+collins.py
+
+This is where the Collins perceptron algorithm is implemented.
+"""
+
+import numpy as np
+import subroutines as sr
+import dataproc as dp
+from ffs import metaff
+
+def main():
+    # first import the training data
+    train_labels = dp.import_labels('dataset/trainingLabels.txt')
+    train_sentences = dp.import_sentences('dataset/trainingSentences.txt')
+
+def collins_epoch(train_labels, train_sentences, w0):
+    """
+    This function is a single epoch of the Collins perceptron.  An epoch ends 
+    after every example in the (shuffled) training data has been visited once.
+    
+    train_labels - A list containing ALL of the labels in the training data.
+    train_sentences - A list of ALL sentences in the training data.
+    w0 - The initial parameter values, at the start of the epoch.
+    """
+    
+    Ntrain = len(train_data)  # number of training examples
+    J = len(w0)  # number of parameters, equal to number of feature functions
+    w = np.zeros(shape=(Ntrain,J))  # to store the parameter trajectory
+    w[0] = w0
+    
+    # need all possible tags
+    tags = ['START','STOP','SPACE','PERIOD','COMMA','COLON','QUESTION_MARK',
+            'EXCLAMATION_PT']
+    
+    for nex,example in enumerate(train_data):
+        # first, calculate g
+        g_ex = sr.g(metaff, w[nex], tags, train_data[1])
+        
+        print g_ex
+
+if __name__ == '__main__':
+    main()
