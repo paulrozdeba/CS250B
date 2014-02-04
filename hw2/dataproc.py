52220710aa60118e11186aebe2d298582a07957e
jaberkow <jackberkowitz88@gmail.com>
2014-02-03 16:45:14 -0800

    added list of labels as ints utility

--- hw2/dataproc.py
+++ hw2/dataproc.py
@@ -36,3 +36,21 @@ def import_labels(path_to_file):
         labels.append(b)
     f.close()
     return labels
+    
+def labels_as_ints(path_to_file):
+    """
+    path_to_file should be the path and filename of the labels data.  Returns
+    a list of lists with corresponding tags as integer equivalents.
+    """
+    key_dict = dict([('SPACE',2),('COMMA',3),('PERIOD',4),('COLON',5),('EXCLAMATION_POINT',6),('QUESTION_MARK',7)])
+    labels = []
+    f = open(path_to_file,'r')
+    for line in f:
+        a = line.replace('\n','')
+        b = a.split()
+        c = [key_dict.get(item,item) for item in b]
+        c += [1]
+        c[:0] = [0]
+        labels.append(c)
+    f.close()
+    return labels
