a187f9da3a913ad908a8bfb53563c2e172357964
Paul Rozdeba <paul.rozdeba@gmail.com>
2014-02-11 18:09:20 -0800

    added pairwise feature functions CORRECTLY, and fixed collins so it updates the weigths CORRECTLY now

--- hw2/ffs.py
+++ hw2/ffs.py
@@ -39,73 +39,69 @@ def sent_precheck(x):
                     'but', 'except', 'if', 'like', 'nor', 'now', 'once', 'or', 
                     'since', 'so', 'than', 'that', 'though', 'unless', 'until', 
                     'when', 'where', 'whether', 'while','for']
+    prepositions = [None,'of','in','to','for','with','on','at','from','by',
+                    'about','as','into','like','through','after','over',
+                    'between','out','against','during','without','before',
+                    'under','around','among']
     suffixes = [None,'able','ible','ial','al','ed','en','er','est','ful','ic','ing',
             'ation','ition','tion','ion','ity','ty','itive','ative','ive','less'
             ,'ly','ment','ness','ious','eous','ous','es']
     prefixes = [None,'anti','de','dis','en','em','fore','intra','inter','in','im','il','ir',
             'mid','mis','non','over','pre','re','semi','sub','super','trans'
             ,'under','un','post']
-    #store the size of these dictionaries for later book keeping
+    
+    # store the size of these dictionaries for later book keeping
     x_info['num_conjunctions'] = len(conjunctions)
+    x_info['num_prepositions'] = len(prepositions)
     x_info['num_suffixes'] = len(suffixes)
     x_info['num_prefixes'] = len(prefixes)
     
-    #allocate arrays for word level checks
+    # allocate arrays for word level checks
     x_conjunctions = np.zeros(len(x), dtype='int')
+    x_prepositions = np.zeros(len(x), dtype='int')
     x_suffixes = np.zeros(len(x), dtype='int')
     x_prefixes = np.zeros(len(x), dtype='int')
     x_capitals = np.zeros(len(x), dtype='int')
     x_firstlast = np.zeros(len(x), dtype='int')
     
-    # First check: capitalization.
+    # First checks: capitalization and FIRSTWORD/LASTWORD:
     for i,word in enumerate(x):
-        if word[0].isupper():
+        if word[0].isupper() and i!=1 and i!=(len(x)-1):
             x_capitals[i] = 1
-        else:
-            x_capitals[i] = 0
-    x_info['capitals'] = x_capitals
-    
-    # Look for FIRSTWORD and LASTWORD
-    for i,word in enumerate(x):
         if word == 'FIRSTWORD':
             x_firstlast[i] = 1
         elif word == 'LASTWORD':
             x_firstlast[i] = 2
+    x_info['capitals'] = x_capitals
     x_info['firstlast'] = x_firstlast
     
     # now lowercase this shit
     x = [word.lower() for word in x]
     
-    #check for interrogative
+    # check for interrogative
     if (x[1] in interrogatives):
         interrogative_test = 1
     else:
         interrogative_test = 0
-    
     x_info['begins_with_interrogative'] = interrogative_test
     
+    # word-level searches
     for i,word in enumerate(x):
-        # check conjunctions
         if word in conjunctions:
             x_conjunctions[i] = conjunctions.index(word)
-    x_info['conjunctions'] = x_conjunctions
-    
-    #check prefixes
-    for i,word in enumerate(x):
+        if word in prepositions:
+            x_prepositions[i] = prepositions.index(word)
         for j in range(1,len(prefixes)):
             if (word.startswith(prefixes[j])):
                 x_prefixes[i] = j
                 break
-    
-    x_info['prefixes'] = x_prefixes
-    
-    #check suffixes
-    for i,word in enumerate(x):
         for j in range(1,len(suffixes)):
             if (word.endswith(suffixes[j])):
                 x_suffixes[i] = j
                 break
-    
+    x_info['conjunctions'] = x_conjunctions
+    x_info['prepositions'] = x_prepositions
+    x_info['prefixes'] = x_prefixes
     x_info['suffixes'] = x_suffixes
     
     return x_info
@@ -127,12 +123,13 @@ def metaff(m1, m2, x_info, i, Jout=False):
     
     # define the size of some spaces
     M = 8  # number of possible tags
-    Np = x_info['num_prefixes']  # number of prefixes
-    Ns = x_info['num_suffixes']  # number of suffixes
-    Nc = x_info['num_conjunctions']  # number of conjunctions
-    Ncap = 2
-    Ninterr = 2
-    Nfl = 3  # FIRSTWORD or LASTWORD class, can be either or none
+    Np = x_info['num_prefixes']-1  # number of prefixes
+    Ns = x_info['num_suffixes']-1  # number of suffixes
+    Nc = x_info['num_conjunctions']-1  # number of conjunctions
+    Nr = x_info['num_prepositions']-1  # number of prepositions
+    Ncap = 1  # capitalized or not
+    Ninterr = 1  # interrogative or not
+    Nfl = 2  # FIRSTWORD or LASTWORD class
     
     ############
     # SINGLE TAG INDICATORS (STI)
@@ -143,38 +140,31 @@ def metaff(m1, m2, x_info, i, Jout=False):
     PREFIX = x_info['prefixes'][i-1:i+1]
     SUFFIX = x_info['suffixes'][i-1:i+1]
     CONJUNCTION = x_info['conjunctions'][i-1:i+1]
+    PREPOSITION = x_info['prepositions'][i-1:i+1]
     CAPITALIZED = x_info['capitals'][i-1:i+1]
     FIRSTLAST = x_info['firstlast'][i-1:i+1]
     ############
     # SENTENCE-LEVEL INDICATORS
     INTERROGATIVE = x_info['begins_with_interrogative']
-    ############
     
     # now start filling out whole list of nonzero feature functions
     TAGS = [STI1, STI2]
-    if i == (L-1):
-        CLASS_SIZES = [Nfl, Ninterr, Np, Ns, Nc, Ncap]
-        ALLIND = [FIRSTLAST, [INTERROGATIVE,INTERROGATIVE], PREFIX, SUFFIX, 
-                  CONJUNCTION, CAPITALIZED]
-    #elif i == 1:
-    #    ALLIND = [FIRSTLAST, PREFIX, SUFFIX, CONJUNCTION, [0,CAPITALIZED[1]]]
-    #    CLASS_SIZES = [Nfl, Np, Ns, Nc, Ncap]
-    else:
-        ALLIND = [FIRSTLAST, PREFIX, SUFFIX, CONJUNCTION, CAPITALIZED]
-        CLASS_SIZES = [Nfl, Np, Ns, Nc, Ncap]
-    
-    #ALLIND = [FIRSTLAST]
+    CLASS_SIZES = [Nfl, Np, Ns, Nc, Nr, Ncap, Ninterr]
+    ALLIND = [FIRSTLAST, PREFIX, SUFFIX, CONJUNCTION, 
+              PREPOSITION, CAPITALIZED, [INTERROGATIVE,INTERROGATIVE]]
     ALLIND_flat = [item for sublist in ALLIND for item in sublist]
+    trueFF = []  # to store indices of true feature functions
     
-    trueFF = []
+    nstart = 0  # start the meta-indexing at 0
     
+    """
     # first do the single-word shit
     # priors on tags
     nstart = 0
-    #trueFF.append(nstart + m1)
-    #nstart += M
-    #trueFF.append(nstart + m2)
-    #nstart += M
+    trueFF.append(nstart + m1)
+    nstart += M
+    trueFF.append(nstart + m2)
+    nstart += M
     
     # now single tag single word indicators
     # THIS IS WHERE SHIT GETS REAL
@@ -187,14 +177,15 @@ def metaff(m1, m2, x_info, i, Jout=False):
         if l != 0:
             trueFF.append(int(nstart + j + (l-1)*M))  # only count true instances
         nstart += int(M * CLASS_SIZES[int(k)])
+    """
     
-    # now pairwise interactions
-    #for (m1,m2),(k,(d1,d2)) in it.product([TAGS],enumerate(ALLIND)):
-    #    if d1!=0 and d2!=0:
-    #        trueFF.append(int(nstart + m1 + m2*M + (d1)*M*M + (d2)*M*M*CLASS_SIZES[k]))
-    #    nstart += int(M + M*M + M*M*CLASS_SIZES[k] + M*M*CLASS_SIZES[k]*CLASS_SIZES[k])
+    # pairwise interactions
+    for (m1,m2),(k,(d1,d2)) in it.product([TAGS],enumerate(ALLIND)):
+        if d1!=0 or d2!=0:
+            trueFF.append(int(nstart + m1 + m2*M + (d1)*M*M + (d2)*M*M*CLASS_SIZES[k]))
+        nstart += int(M + M*M + M*M*CLASS_SIZES[k] + M*M*CLASS_SIZES[k]*CLASS_SIZES[k])
     
-    J = nstart
+    J = nstart  # the total number of feature functions
     
     if Jout == False:
         return trueFF
