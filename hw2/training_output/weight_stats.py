448b663f2cd94e4355ae4f98845e1c657d4fb7ee
Paul Rozdeba <paul.rozdeba@gmail.com>
2014-02-12 22:11:15 -0800

    added score results

--- hw2/training_output/weight_stats.py
+++ hw2/training_output/weight_stats.py
@@ -22,14 +22,17 @@ pc_0p25 = stats.pearsonr(collins_0p25, sgd_0p25)
 pc_0p5 = stats.pearsonr(collins_0p5, sgd_0p5)
 pc_0p75 = stats.pearsonr(collins_0p75, sgd_0p75)
 
-filenames = ['scatter_0p25.pdf','scatter_0p5.pdf','scatter_0p75.pdf']
+filenames = ['scatter_0p25.png','scatter_0p5.png','scatter_0p75.png']
+titles = [r'.25/.75 training/validation', r'.50/.50 training/validation', 
+            r'.75/.25 training/validation']
 
 # plot scatter
-for fname,coll,sgd in zip(filenames, [collins_0p5,collins_0p75],[sgd_0p5,sgd_0p75]):
+for fname,tit,coll,sgd in zip(filenames, titles, [collins_0p25,collins_0p5,collins_0p75],[sgd_0p25,sgd_0p5,sgd_0p75]):
     fig = plt.figure()
     ax = fig.add_subplot(1,1,1)
     ax.scatter(sgd, coll, s=1)
     ax.set_xlabel(r'$\bar{w}$ (SGD)')
     ax.set_ylabel(r'$\bar{w}$ (Collins)')
-    ax.set_title(r'.75/.25 training/validation')
+    ax.set_title(tit)
     fig.savefig(fname)
+    plt.close(fig)
