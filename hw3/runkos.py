f6320789ab1a45f9007774c8101468124d1b4758
Paul Rozdeba <paul.rozdeba@gmail.com>
2014-02-24 21:21:36 -0800

    runkos ready to run

--- hw3/runkos.py
+++ hw3/runkos.py
@@ -7,21 +7,23 @@ Runs topic modeling on dailykos.com data set (source: UCI MLR)
 import numpy as np
 import scipy.io
 import gibbs
+import sys
 
 # Load in the data
 kosdata = np.loadtxt('data/docword.kos.txt', dtype='float', skiprows=3)
 
 # split into counts and indices
-doc_idx = kosdata[:10000,0] - 1
-voc_idx = kosdata[:10000,1] - 1
-counts  = kosdata[:10000,2]
+doc_idx = kosdata[:,0] - 1
+voc_idx = kosdata[:,1] - 1
+counts  = kosdata[:,2]
 
 M = 3431  # number of documents
 V = 6907  # size of vocabulary
 S = len(counts)  # number of nonzero elements
 
 # initialize hyperparameters
-K = 3
+#K = 3
+K = int(sys.argv[1])
 alpha = 1.0 * np.ones(K)
 beta  = 1.0 * np.ones(V)
 
@@ -44,7 +46,7 @@ for bi,(m,count) in enumerate(zip(doc_idx,counts)):
 
 q,n = gibbs.gibbs_epoch(q,n,alpha,beta,doc_idx,voc_idx)
 
-qfname = 'data/kos_q_a1p0_b1p0_K3.dat'
-nfname = 'data/kos_n_a1p0_b1p0_K3.dat'
+qfname = 'data/kos_q_a1p0_b1p0_K'+str(K)+'.dat'
+nfname = 'data/kos_n_a1p0_b1p0_K'+str(K)+'.dat'
 np.savetxt(qfname,np.array(q),fmt='%d')
 np.savetxt(nfname,np.array(n),fmt='%d')
