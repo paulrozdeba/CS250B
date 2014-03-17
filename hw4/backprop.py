fixed indexing issue in backprop_full, where right and left leaf indicators werent resetting after iterations

hw4/backprop.py
@@ -500,6 +500,8 @@ def backprop_core_full(tree, treeinfo, t, h, Dh, g, Dg, pars, alpha=0.2):
                 if cidx == treeinfo[treeinfo[cidx,2],1]:
                     isright = True
                 # are the children leaves?
+                leftLeaf = False
+                rightLeaf = False
                 if treeinfo[treeinfo[cidx,0],0] == (2*N-1):
                     leftLeaf = True
                 if treeinfo[treeinfo[cidx,1],0] == (2*N-1):
@@ -547,9 +549,9 @@ def backprop_core_full(tree, treeinfo, t, h, Dh, g, Dg, pars, alpha=0.2):
                           np.einsum('ij,j',Wleft,c_deltax2)
                     Dx[lchild2_idx] += Dx1 + Dx2
                 if rightLeaf == True:
-                    Dx1 = alpha * np.einsum('ij,j',Wright[:,:-1],c_deltax1)
+                    Dx1 = alpha * np.einsum('ij,j',Wright,c_deltax1)
                     Dx2 = (1.0-alpha) * \
-                          np.einsum('ij,j',Wright[:,:-1],c_deltax2)
+                          np.einsum('ij,j',Wright,c_deltax2)
                     Dx[rchild2_idx] += Dx1 + Dx2
             
             # If you've reached this point, you've look at all the children of

hw4/backprop_tests.py
@@ -20,9 +20,9 @@ def Dg(x,V):
 
 def runtest():
     # invent dummy tree and stuff
-    DM = 2
+    DM = 5
     DL = 2
-    Nw = 4
+    Nw = 42
     Nn = 2*Nw - 1
     """
     # two-word tree
@@ -44,6 +44,7 @@ def runtest():
                          [3, 2, Nn, 3]]).T
     """
     # four-word tree
+    """
     tree = np.random.randn(Nn,DM).T
     treeinfo = np.array([[Nn,Nn,4,1],
                          [Nn,Nn,4,1],
@@ -52,6 +53,11 @@ def runtest():
                          [0,1,6,2],
                          [2,3,6,2],
                          [4,5,Nn,4]]).T
+    """
+    tree = np.load('meanings.npy')
+    treeinfo = np.load('structure.npy')
+    print tree.T
+    print treeinfo.T
     t = np.random.randn(DL)
 
     # initialize parameters