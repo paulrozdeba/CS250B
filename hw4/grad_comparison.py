80a06ce6e9e0ca153d644f55900267c93f3e4fae
Paul Rozdeba <paul.rozdeba@gmail.com>
2014-03-17 08:09:06 +0530

    grad_comparison is beefier now, not quite done. bug fix to training, counting issue

--- hw4/grad_comparison.py
+++ hw4/grad_comparison.py
@@ -7,6 +7,9 @@ an estimate calculated using a central difference approximation.
 
 import numpy as np
 import matplotlib.pyplot as plt
+from backprop import backprop,backprop_full
+from training import full_j
+from dataproc import format_data
 
 def midpoint_grad(f, x, eps):
     """
@@ -32,6 +35,39 @@ def main():
     b2 = np.random.randn(2*D)
     Wlabel = np.random.randn(L,D)
     
+    # flattened arrays
+    W1flat = W1.flatten()
+    W2flat = W2.flatten()
+    Wlabelflat = Wlabel.flatten()
+    allflat = np.append(W1flat,b1)
+    allflat = np.append(allflat,W2flat)
+    allflat = np.append(allflat,b2)
+    allflat = np.append(allflat,Wlabelflat)
+    Np = len(allflat)
+    
+    # hyperparameters
+    eps = 0.0001
+    lambda_reg = 0.1
+    alpha = 0.2
+    
+    # get the data
+    neg_list,pos_list = format_data()
+    neg_list = neg_list[:1]
+    pos_list = pos_list[:1]
+    vocab = np.random.randn(268810,D)
+    
+    numgrad = np.zeros(Np)
+    for i in range(Np):
+        print 'P ' + str(i)
+        allflat[i] += eps
+        fxpe = full_j(allflat,D,L,lambda_reg,alpha,neg_list,pos_list,
+                      vocab,normalized=True)
+        allflat[i] -= 2.0*eps
+        fxme = full_j(allflat,D,L,lambda_reg,alpha,neg_list,pos_list,
+                       vocab,normalized=True)
+        allflat[i] += eps
+        numgrad[i] = (fxpe - fxme)/(2.0*eps)
+    print numgrad
     
 
 if __name__ == '__main__':
